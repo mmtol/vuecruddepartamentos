f771dd6546a37df302665c00adef681f7549e177
mmtol <mmigueltolosana@gmail.com>
2025-11-04 16:37:26 +0100

    fin

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import DepartamentosComponent from './components/DepartamentosComponent.vue';
 import HomeComponent from './components/HomeComponent.vue';
 import CreateDept from './components/CreateDept.vue';
 import DetallesDept from './components/DetallesDept.vue';
+import DeleteDept from './components/DeleteDept.vue';
 import UpdateDept from './components/UpdateDept.vue';
 
 const rutas =
@@ -26,6 +27,10 @@ const rutas =
     {
         path:"/update/:numero",
         component:UpdateDept
+    },
+    {
+        path:"/delete/:numero",
+        component:DeleteDept
     }
 ]
 
